623cd24e01ff4aba8f55c654b759dd92bac8921b
William Flinchbaugh <Williamflinchbaugh@gmail.com>
2025-09-20 23:51:53 -0500

    updated database connection for new tables

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 import boto3
+from boto3.dynamodb.conditions import Key
 import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
@@ -6,34 +7,31 @@ from datetime import datetime, timedelta
 import time
 from data_generation import generate_cell_tower_data, generate_predicted_outages
 import uuid
-
+from utils import n_towers_within_zone
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
+load_dotenv()
+
 # --- AWS DynamoDB setup ---
-# dynamodb = boto3.resource(
-#     "dynamodb",
-#     region_name="us-east-1",  # change region if needed
-#     aws_access_key_id=os.getenv("key"),     # load from env variables
-#     aws_secret_access_key=os.getenv("key")
-# )
+dynamodb = boto3.resource(
+    "dynamodb",
+    region_name="us-east-1", # change region if needed
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY"), # load from env variables
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+)
 
-# # Change this to your DynamoDB table name
-# weather_table = dynamodb.Table("AWS-weather-data")
-# network_table = dynamodb.Table("network-status-data")
-# weather_outages_table = dynamodb.Table("weatheroutages")
-# network_outages_table = dynamodb.Table("networkoutages")
+cell_towers_table = dynamodb.Table("cell-towers")
+outages_table = dynamodb.Table("outages")
 
-# bedrock_agent_runtime_client = boto3.client('bedrock-agent-runtime')
+bedrock_agent_runtime_client = boto3.client('bedrock-agent-runtime', region_name='us-east-1')
 
-# # Define the parameters for invoking the agent
-# agent_id = "GHZTFEK4B4"  # Replace with your agent ID
-# agent_alias_id = "WBJRP03YGP"  # Replace with your agent alias ID
-# session_id = "UNIQUE_SESSION_ID"  # A unique ID for the conversation session
-# input_text = "What is the current status of my order?" # The user's prompt
-
-
+# Define the parameters for invoking the agent
+agent_id = "GHZTFEK4B4"  # Replace with your agent ID
+agent_alias_id = "WBJRP03YGP"  # Replace with your agent alias ID
+session_id = "UNIQUE_SESSION_ID"  # A unique ID for the conversation session
 
 
 # API Routes
@@ -42,15 +40,13 @@ def health_check():
     """Health check endpoint"""
     return jsonify({'status': 'healthy', 'timestamp': datetime.now().isoformat()})
 
+
 @app.route('/api/towers', methods=['GET'])
 def get_towers():
     try:
-        towers_data = generate_cell_tower_data()
-        # response = network_table.scan()
-        # items = response.get("Items", [])
+        response = cell_towers_table.scan()
+        towers_data = response.get("Items", [])
 
-
-        # return jsonify({'success': True, 'data': items})
         return jsonify({'success': True, 'data': towers_data})
     except Exception as e:
         return jsonify({'success': False, 'error': str(e)}), 500
@@ -59,13 +55,13 @@ def get_towers():
 @app.route('/api/predicted-outages', methods=['GET'])
 def get_predicted_outages():
     try:
-        outages_data = generate_predicted_outages()
-        # response = weather_outages_table.scan()
-        # outages_data = response.get("Items", [])
-
+        response = outages_table.scan()
+        outages_data = response.get("Items", [])
+        
         return jsonify({'success': True, 'data': outages_data})
     except Exception as e:
         return jsonify({'success': False, 'error': str(e)}), 500
+
 
 @app.route('/api/deployment-notes', methods=['GET'])
 def get_deployment_notes():
@@ -78,16 +74,6 @@ def get_deployment_notes():
     except Exception as e:
         return jsonify({'success': False, 'error': str(e)}), 500
 
-# @app.route('/api/instructions-outage', methods=['GET'])
-# def get_instructions_for_outage():
-#     try:
-#         # TODO: find outage info in DB, give to agent, return response
-#         outage_id = request.args.get('outage_id', default=None, type=str)
-#         instructions_response = f"Here are the instructions for outage {outage_id}..."
-        
-#         return jsonify({'success': True, 'data': instructions_response})
-#     except Exception as e:
-#         return jsonify({'success': False, 'error': str(e)}), 500
 
 @app.route('/api/assistant', methods=['GET'])
 def get_assistant_response():
@@ -100,50 +86,6 @@ def get_assistant_response():
         return jsonify({'success': True, 'data': assistant_response})
     except Exception as e:
         return jsonify({'success': False, 'error': str(e)}), 500
-
-# @app.route('/api/assistant-outage', methods=['POST'])
-# def get_assistant_response_for_outage():
-#     #try:
-#         # TODO: find tower info in DB, give that and the input to agent, return response
-#         #user_input = request.args.get('user_input', default=None, type=str)
-#         #outage_id = request.args.get('outage_id', default=None, type=str)
-#         #assistant_response = f"Here is my response to '{user_input}' for outage {outage_id}..."
-
-#     try:
-
-#         user_input = request.args.get('user_input', default=None, type=str)
-#         outage_id = request.args.get('outage_id', default=None, type=str)
-#         data = request.json
-#         #user_input = data.get("inputText")
-        
-#         if not user_input:
-#             return jsonify({"status": "error", "message": f"user input not valid: f{user_input}"}), 400
-
-#         # Generate or reuse sessionId (frontend can also pass it in if you want persistent chat)
-#         session_id = data.get("sessionId", str(uuid.uuid4()))
-
-#         # Call the agent
-#         response = client.invoke_agent(
-#             agentId=GHZTFEK4B4,
-#             agentAliasId=WBJRP03YGP,
-#             sessionId=session_id,
-#             inputText=user_input
-#         )
-
-#         # Extract text from agent’s response
-#         messages = []
-#         for event in response["completion"]:
-#             if "chunk" in event:
-#                 messages.append(event["chunk"]["bytes"].decode("utf-8"))
-
-#         assistant_response = "".join(messages)
-
-#         return jsonify({'success': True, 'data': assistant_response})
-
-#     except Exception as e:
-#         return jsonify({'success': False, 'error': str(e)}), 500
-
-
 
 
 
